4ce55a88e523dc6f96ad4590aefd2e759526b79b
enzechua <1700951@sit.singaporetech.edu.sg>
2021-05-07 13:20:28 +0800

    Added Transaction add page

--- dbsseed/src/App.js
+++ dbsseed/src/App.js
@@ -3,9 +3,22 @@ import "./App.css";
 import Main from "./components/dashboard/Main";
 import Navbar from "./components/navbar/Navbar";
 import Sidebar from "./components/sidebar/Sidebar";
-import {
+import AddTransactionPage from "./pages/AddTransaction/"
+import axios from 'axios'
+ import {
   BrowserRouter as Router
 } from 'react-router-dom'
+
+
+// axios.interceptors.request.use(
+//   config => {
+//     config.headers["x-api-key"] = 'BDTv5isstyQr8aGI2xNp5CzqSYFCrtXPIAZV4Z40'
+//     return config;
+//   },
+//   error => {
+//     return Promise.reject(error)
+//   }
+// )
 
 const App = () => {
   const [sidebarOpen, setsidebarOpen] = useState(false);
@@ -19,8 +32,8 @@ const App = () => {
     <Router>
       <div className="root-container">
         <Navbar sidebarOpen={sidebarOpen} openSidebar={openSidebar} />
-        <Main />
         <Sidebar sidebarOpen={sidebarOpen} closeSidebar={closeSidebar} />
+        <AddTransactionPage />
       </div>
     </Router>
   );
